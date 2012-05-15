8faf00a636cf131069158969cddf95aeeef6d396
Bernardo Fontes <falecomigo@bernardofontes.net>
2012-05-15 12:51:53 -0300

    Implementa pipeline como prova de conceito para a escrita de arquivo no dropbox

--- camiseteria/camiseteria/pipelines.py
+++ camiseteria/camiseteria/pipelines.py
@@ -1,3 +1,15 @@
+import simplejson as json
+
 class CamiseteriaPipeline(object):
+
+    def __init__(self):
+        self.dict_items = []
+
     def process_item(self, item, spider):
+        self.dict_items.append(dict(item))
         return item
+
+    def close_spider(self, spider):
+        json_content = json.dumps(self.dict_items)
+        with open('output.json', 'w') as fp:
+            fp.write(json_content)
--- camiseteria/camiseteria/settings.py
+++ camiseteria/camiseteria/settings.py
@@ -4,3 +4,5 @@ BOT_VERSION = '1.0'
 SPIDER_MODULES = ['camiseteria.spiders']
 NEWSPIDER_MODULE = 'camiseteria.spiders'
 USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+
+ITEM_PIPELINES = ['camiseteria.pipelines.CamiseteriaPipeline']
