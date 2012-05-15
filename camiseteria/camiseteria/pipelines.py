ff0b2d1a1a56f74384ba69fed11586ca419f717e
Bernardo Fontes <falecomigo@bernardofontes.net>
2012-05-15 13:45:17 -0300

    Enviando arquivo json resultante do crawler para o Dropbox

--- camiseteria/camiseteria/pipelines.py
+++ camiseteria/camiseteria/pipelines.py
@@ -1,4 +1,7 @@
+import os
 import simplejson as json
+
+from .dbconn import DropboxConnection
 
 class CamiseteriaPipeline(object):
 
@@ -10,6 +13,17 @@ class CamiseteriaPipeline(object):
         return item
 
     def close_spider(self, spider):
+        conn = self._get_dropbox_connection()
+
         json_content = json.dumps(self.dict_items)
-        with open('output.json', 'w') as fp:
+        tmp_file = '/tmp/out.json'
+        with open(tmp_file, 'w') as fp:
             fp.write(json_content)
+
+        file_name = '%s.json' % spider.name
+        conn.upload_file(tmp_file, '/Public/', file_name)
+
+    def _get_dropbox_connection(self):
+        db_user = 'bernardo@yourjetpack.com.br'
+        db_pass = os.environ.get('DROPBOX_PASSWD')
+        return DropboxConnection(db_user, db_pass)
